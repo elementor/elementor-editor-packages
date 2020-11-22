011c4e7d4603893588a89f3e86ba1a08dc31b3f7
Nevo Golan <nevog@elementor.com>
2020-11-22 15:52:04 +0200

    feat(git-hooks): init the package + create php-lint pre-commit

---
+++ packages/git-hooks/src/scripts/lint-js.js
@@ -0,0 +1,5 @@
+import { success } from '../utils/logs';
+
+export async function lintJs() {
+	success('lint-js passed!');
+}
---
+++ packages/git-hooks/src/scripts/lint-php.js
@@ -0,0 +1,42 @@
+import { exec } from 'shelljs';
+import { error, exit, log, success } from '../utils/logs';
+
+export async function lintPhp() {
+	log('Start linting php files.');
+
+	const fileDiff = exec(
+		'git diff-index --cached --name-only HEAD | grep .php',
+		{
+			silent: true,
+		}
+	);
+
+	if (!fileDiff.stdout) {
+		log('No files to lint.');
+		exit();
+	}
+
+	const composerExists = exec('which composer', {
+		silent: true,
+	});
+
+	if (composerExists.code !== 0) {
+		error(
+			'composer is not exists. please install composer and run `composer install`.'
+		);
+	}
+
+	const files = fileDiff.stdout.toString().split('\n').join(' ');
+
+	const lint = exec(`composer run lint ${files}`);
+
+	if (lint.code === 1) {
+		error('`lint` script is not defined in composer.json file.');
+	}
+
+	if (lint.code === 2) {
+		error('There are some lint errors, please fix them.', 2);
+	}
+
+	success('lint-php passed!');
+}
