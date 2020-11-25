6d04c2747eb85741edabdc446d022b53d2b08c74
Nevo Golan <nevog@elementor.com>
2020-11-25 16:56:28 +0200

    feat(git-hooks): add lint-php, lint-js and pre-commit scripts

---
+++ packages/git-hooks/src/scripts/lint-js.js
@@ -0,0 +1,35 @@
+import { error, log, success } from '../utils/logs';
+import { getStagedFiles } from '../utils/git';
+import { exec } from 'shelljs';
+
+export async function lintJs() {
+	log('Start linting js files.');
+
+	const stagedFiles = getStagedFiles('js');
+
+	if (!stagedFiles.length) {
+		log('No files to lint.');
+
+		return;
+	}
+
+	const composerExists = exec('which npm', {
+		silent: true,
+	});
+
+	if (composerExists.code !== 0) {
+		error('npm is not exists. please install npm and run `npm install`.');
+	}
+
+	const files = stagedFiles.join(' ');
+
+	const lint = exec(`npm run lint -- ${files}`);
+
+	if (lint.code === 1) {
+		error(
+			'There are some lint errors, please fix them OR `lint` script is not defined in packages.json file.'
+		);
+	}
+
+	success('lint-js passed!');
+}
