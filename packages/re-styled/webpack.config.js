eba23cd09ce59ccd873d885570d57ef6a0f70018
Rotem Ben-Itzhak <rotem@elementor.com>
2021-08-17 08:14:07 +0300

    feat(re-styled): adding a new styled-components package

---
+++ packages/re-styled/webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require( 'webpack' );
+const path = require( 'path' );
+
+module.exports = {
+	mode: 'production',
+	entry: './src/index.js',
+	output: {
+		filename: 'index.js',
+		library: '@elementor/re-styled',
+		libraryTarget: 'umd',
+		path: path.resolve( __dirname, 'lib' ),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [ [ '@babel/preset-env', { "useBuiltIns": "usage" } ], '@babel/preset-react' ],
+						},
+					},
+				],
+			},
+		],
+	},
+};
