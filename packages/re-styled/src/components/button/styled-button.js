31675507ceb6a34dc8c7d5223977dd46645ee56c
Rotem Ben-Itzhak <rotem@elementor.com>
2021-08-18 17:46:42 +0300

    feat(re-styled): testing

--- packages/re-styled/src/components/button/styled-button.js
+++ packages/re-styled/src/components/button/styled-button.js
@@ -1,3 +1,14 @@
-export default function Test() {
-	return (<p>Test Component</p>);
-}
+import styled from 'styled-components';
+
+const Button = styled.div`
+	display: inline-flex;
+	color: orange;
+	background-color: blue;
+	line-height: 1;
+	cursor: pointer;
+	padding: 10px;
+`;
+
+export const StyledButton = ( props ) => (
+	<Button { ...props } className="eps-button">{ props.children }</Button>
+);
