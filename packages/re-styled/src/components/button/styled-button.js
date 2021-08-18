42572c7a5d9769e32b61bd74ebbd1ed0ae94a368
Rotem Ben-Itzhak <rotem@elementor.com>
2021-08-18 15:59:03 +0300

    feat(re-styled): testing the button component

--- packages/re-styled/src/components/button/styled-button.js
+++ packages/re-styled/src/components/button/styled-button.js
@@ -1,20 +1,3 @@
-import styled from 'styled-components';
-
-const Button = styled.div`
-	display: inline-flex;
-	color: white;
-	background-color: blue;
-	line-height: 1;
-	cursor: pointer;
-	padding: 10px;
-`;
-
-export const StyledButton = ( props ) => (
-	<Button { ...props } className="eps-button">{ props.children }</Button>
-);
-
-export const Element = Button;
-
-StyledButton.propTypes = {
-	children: PropTypes.any,
+export default () => {
+	return (<p>ROTEM</p>);
 };
--- packages/re-styled/webpack.config.js
+++ packages/re-styled/webpack.config.js
@@ -4,9 +4,6 @@ const path = require( 'path' );
 module.exports = {
 	mode: 'production',
 	entry: './src/index.js',
-	externals: {
-		react: 'React',
-	},
 	resolve: {
 		alias: {
 			'utils': path.resolve( __dirname, './src/utils/' ),
