03a0214efac8000a6e3a4a49384892bab91f471b
Rotem Ben-Itzhak <rotem@elementor.com>
2021-08-22 20:52:25 +0300

    feat(styles): adding new package for the generic styles

---
+++ packages/styles/webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require( 'webpack' );
+const path = require( 'path' );
+
+module.exports = {
+	mode: 'production',
+	entry: './src/index.js',
+	resolve: {
+		alias: {
+			'utils': path.resolve( __dirname, './src/utils/' ),
+			'theme': path.resolve( __dirname, './src/components/theme' ),
+		},
+	},
+	output: {
+		filename: 'index.js',
+		library: '@elementor/styles',
+		libraryTarget: 'umd',
+		path: path.resolve( __dirname, 'lib' ),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [ [ '@babel/preset-env', { "useBuiltIns": "usage", "corejs": 3 } ], '@babel/preset-react' ],
+						},
+					},
+				],
+			},
+		],
+	},
+	externals: {
+		// Don't bundle react or react-dom
+		react: {
+			commonjs: "react",
+			commonjs2: "react",
+			amd: "React",
+			root: "React"
+		},
+		"react-dom": {
+			commonjs: "react-dom",
+			commonjs2: "react-dom",
+			amd: "ReactDOM",
+			root: "ReactDOM"
+		}
+	}
+};
