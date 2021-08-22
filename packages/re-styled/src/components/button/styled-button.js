66e0d2a3fc4f6ccf5106232422f3993937c44fa0
Rotem Ben-Itzhak <rotem@elementor.com>
2021-08-22 15:06:52 +0300

    feat(re-styled): adding theme to button

--- packages/re-styled/src/components/button/styled-button.js
+++ packages/re-styled/src/components/button/styled-button.js
@@ -1,14 +1,26 @@
 import styled from 'styled-components';
+import Theme from 'theme';
+import { themeColors } from 'utils';
+
+const defaultTheme = {
+	backgroundColor: themeColors( 'info' ),
+};
+
+const darkTheme = {
+	backgroundColor: themeColors( 'warning' ),
+};
 
 const Button = styled.div`
 	display: inline-flex;
-	color: orange;
-	background-color: blue;
+	color: white;
+	background-color: ${ ( props ) => props.theme.backgroundColor };
 	line-height: 1;
 	cursor: pointer;
 	padding: 10px;
 `;
 
 export const StyledButton = ( props ) => (
-	<Button { ...props } className="eps-button">{ props.children }</Button>
+	<Theme default={ defaultTheme } dark={ darkTheme }>
+		<Button { ...props } className="eps-button">{ props.children }</Button>
+	</Theme>
 );
