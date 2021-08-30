0bc7e4ff2761beddb5b8b7e408e4ebe9e6de0e28
Rotem Ben-Itzhak <rotem@elementor.com>
2021-08-30 10:57:41 +0300

    feat(test): testing

--- packages/re-styled/src/components/heading/styled-heading.js
+++ packages/re-styled/src/components/heading/styled-heading.js
@@ -1,9 +1,4 @@
 import styled from 'styled-components';
-import { default as Rotem } from '@elementor/styles';
-import breakpoints from '@elementor/styles';
-
-console.log( '### Rotem 444', Rotem );
-console.log( '### breakpoints 444 - deconstruct', breakpoints );
 
 const Heading = styled.h1`
 	color: red;
