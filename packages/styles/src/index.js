acb32f2771a9492c452bc1d4c4e733586da9b5d6
Rotem Ben-Itzhak <rotem@elementor.com>
2021-08-30 09:27:56 +0300

    feat(styles): test

--- packages/styles/src/index.js
+++ packages/styles/src/index.js
@@ -1,3 +1,27 @@
-const breakpoints = require( './maps/breakpoints/breakpoints.js' );
+const breakpointsMap = require( './maps/breakpoints/breakpoints.js' );
+const themeColorsMap = require( './maps/colors/theme-colors' );
+const tintsMap = require( './maps/colors/tints' );
+const spacingMap = require( './maps/spacing/spacing' );
 
-module.exports = breakpoints;
+console.log( 'rotem - index - styles ------------------------------- 333' );
+
+exports.breakpoints = breakpointsMap;
+
+exports.themeColors = ( key ) => {
+	return themeColorsMap[ key ].hex;
+};
+
+exports.tints = ( key ) => {
+	return tintsMap[ key ].hex;
+};
+
+exports.selectors = {
+	base: '_base',
+	dark: '.eps-theme-dark',
+};
+
+exports.spacing = ( key ) => {
+	return spacingMap.values[ key ] && ( spacingMap.values[ key ] * spacingMap.base.spacer ) + spacingMap.base.units;
+};
+
+exports.heading = require( './variants/heading' );
