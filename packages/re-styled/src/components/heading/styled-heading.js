176e8d650e31e4657a199cfa0978c99730616041
Rotem Ben-Itzhak <rotem@elementor.com>
2021-08-30 09:48:32 +0300

    feat(re-styled): test

--- packages/re-styled/src/components/heading/styled-heading.js
+++ packages/re-styled/src/components/heading/styled-heading.js
@@ -4,9 +4,9 @@ import { default as rotem } from '@elementor/styles';
 import * as rotem2 from '@elementor/styles';
 import breakpoints from '@elementor/styles';
 
-console.log( '### rotem888', rotem );
+console.log( '### rotem999', rotem );
 console.log( '### rotem999-3', rotem2 );
-console.log( '### breakpoints3', breakpoints );
+console.log( '### breakpoints999', breakpoints );
 
 const Heading = styled.h1`
 	${ ( props ) => getVariant( props.variant, {} ) }
