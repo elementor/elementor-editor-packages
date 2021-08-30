77f595a8b9ea485648d5651085f5a5e56320509e
Rotem Ben-Itzhak <rotem@elementor.com>
2021-08-30 13:06:08 +0300

    feat(test): testing

--- packages/styles/src/index.js
+++ packages/styles/src/index.js
@@ -2,12 +2,14 @@ import breakpointsMap from './maps/breakpoints/breakpoints.js';
 import themeColorsMap from './maps/colors/theme-colors';
 import tintsMap from './maps/colors/tints';
 import spacingMap from './maps/spacing/spacing';
+import * as variantsData from './variants';
 
 export const breakpoints = breakpointsMap;
 
 export const themeColors = ( key ) => {
 	return themeColorsMap[ key ].hex;
 };
+
 export const tints = ( key ) => {
 	return tintsMap[ key ].hex;
 };
@@ -16,6 +18,9 @@ export const selectors = {
 	base: '_base',
 		dark: '.eps-theme-dark',
 };
+
 export const spacing = ( key ) => {
 	return spacingMap.values[ key ] && ( spacingMap.values[ key ] * spacingMap.base.spacer ) + spacingMap.base.units;
 };
+
+export const variants = variantsData;
